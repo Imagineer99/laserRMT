2b65176bcf588ce809185588da69fcd29282e2e9
Lee Jackson <130007945+Imagineer99@users.noreply.github.com>
2024-02-17 03:57:33 +0000

    Update rmt_laser_snr_math.py

--- rmt_laser_snr_math.py
+++ rmt_laser_snr_math.py
@@ -1,9 +1,10 @@
+# %%
+model_name = "cognitivecomputations/dolphin-2.6-mistral-7b-dpo"  # Change to your preferred model
+
+# %%
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
-import datasets
-from lib.utils import gptq_data_utils_math
 from tqdm import tqdm
-import random
 import numpy as np
 import gc
 
@@ -15,8 +16,7 @@ class ModelModifier:
         self.layer_snr = {}
         self.modified_layers = set()
         self.original_weights = {}
-  
-        
+
     def calculate_snr_for_layer(self, layer_type, layer_number):
         for name, module in self.model.named_modules():
             if layer_type in name and str(layer_number) in name:
@@ -36,6 +36,20 @@ class ModelModifier:
                 gc.collect()
                 return snr
 
+    @staticmethod
+    def marchenko_pastur_threshold(sigma, n, m):
+        beta = n / m if n < m else m / n
+        threshold = sigma * np.sqrt((1 + np.sqrt(beta))**2)
+        return threshold
+
+    @staticmethod
+    def estimate_sigma_with_full_iqr(S):
+        q75 = torch.quantile(S, 0.75, dim=-1)
+        q25 = torch.quantile(S, 0.25, dim=-1)
+        iqr = q75 - q25
+        sigma_estimated = iqr / 1.349
+        return sigma_estimated
+
     def update_model_reduce_layer(self, layer_type, layer_number):
         layer_id = f"{layer_type}_{layer_number}"
         if layer_id in self.modified_layers:
@@ -50,54 +64,32 @@ class ModelModifier:
                 weights = module.weight.double()
                 U, S, V = torch.linalg.svd(weights, full_matrices=False)
 
-                # Estimate sigma using the full IQR method
                 sigma_estimated_full_iqr = self.estimate_sigma_with_full_iqr(S)
-
-                # Calculate Marchenko-Pastur threshold
                 n, m = weights.shape
                 mp_threshold_full_iqr = self.marchenko_pastur_threshold(sigma_estimated_full_iqr, n, m)
 
-                # Retain only the singular values above the MP threshold
-                S_reduced = torch.zeros_like(S)
-                k = (S > mp_threshold_full_iqr).sum().item()
-                S_reduced[:k] = S[:k]
+                S_reduced = S[S > mp_threshold_full_iqr]
+                k = len(S_reduced)
+                S[:k] = S_reduced
+                S[k:] = 0
                 print(f"Reduced from {S.shape} to {k}")
 
-                # Reconstruct the matrix using the thresholded singular values
-                reconstructed_weights = U @ torch.diag(S_reduced) @ V
+                reconstructed_weights = U @ torch.diag(S) @ V
                 reconstructed_weights = reconstructed_weights.to(original_dtype)
                 module.weight = torch.nn.Parameter(reconstructed_weights)
                 self.modified_layers.add(layer_id)
                 return True
 
-    @staticmethod
-    def marchenko_pastur_threshold(sigma, n, m):
-        beta = n / m if n < m else m / n
-        threshold = sigma * np.sqrt((1 + np.sqrt(beta))**2)
-        return threshold
-    
-    ## Calculate an estimate of the standard deviation of the singular values based on Inter Quantile Range
-
-    @staticmethod
-    def estimate_sigma_with_full_iqr(S):
-        q75 = torch.quantile(S, 0.75)
-        q25 = torch.quantile(S, 0.25)
-        iqr = q75 - q25
-        sigma_estimated = iqr / 1.349 ## 0.6745 * sigma is the expected range between the quantiles (Q1 and Q3)
-        return sigma_estimated
-
-
     def restore_model_original_layer(self, layer_type, layer_number):
-        layer_id = f"{layer_type}_{layer_number}"
-        for name, module in self.model.named_modules():
-            if layer_type in name and layer_number in name:
-                if name in self.original_weights:
-                    module.weight = torch.nn.Parameter(self.original_weights[name])
-                    print(f"Restored original weights for layer: {name}")
-                    if layer_id in self.modified_layers:
-                        self.modified_layers.remove(layer_id)
-                else:
-                    print(f"No original weights saved for layer: {name}")
+        layer_name = f"{layer_type}_{layer_number}"
+        if layer_name in self.original_weights:
+            module = getattr(self.model, layer_name)
+            module.weight = torch.nn.Parameter(self.original_weights[layer_name])
+            print(f"Restored original weights for layer: {layer_name}")
+            if layer_name in self.modified_layers:
+                self.modified_layers.remove(layer_name)
+        else:
+            print(f"No original weights saved for layer: {layer_name}")
 
     def calculate_model_perplexity(self, datasets=['gsm8k'], seqlen=32, use_cuda_graph=False, use_flash_attn=False):
         model = self.model
@@ -112,14 +104,10 @@ class ModelModifier:
             rest = total_length % seqlen
 
             if rest != 0:
-            # if the last part of the data is not complete, we cut it off
                 input_tok = input_tok[:-rest]
 
-            input_tok = input_tok.view(-1, seqlen)  # reshape the tensor
+            input_tok = input_tok.view(-1, seqlen)
             total_samples += nsamples
-
-            #if not use_cuda_graph:
-            #    model.reset()
 
             loss_fct = torch.nn.CrossEntropyLoss().cuda()
             progress = tqdm(range(nsamples))
@@ -135,8 +123,6 @@ class ModelModifier:
         avg_loss = acc_loss / total_samples
         ppl = torch.exp(torch.tensor(avg_loss)).item()
         return ppl
-    
-
 
     def assess_layers_snr(self, layer_types, layer_numbers):
         for name, _ in self.model.named_modules():
@@ -144,57 +130,85 @@ class ModelModifier:
                 for layer_type in layer_types:
                     if layer_type in name and str(layer_number) in name:
                         layer_name = f"{layer_type}+{layer_number}"
-                        print("*"*50, flush=True)
+                        print("*" * 50, flush=True)
                         print(f"Calculating Signal to Noise Ratio at layer {layer_name}", flush=True)
+
+                        # Additional RMT complexity: Calculate the average singular value
+                        average_s_value = self.average_singular_value(S)
+                        print(f"Average Singular Value at layer {layer_name}: {average_s_value}", flush=True)
+
+                        # Additional RMT complexity: Calculate the Tracy-Widom distribution
+                        tracy_widom_values = self.tracy_widom_distribution(beta=1, size=len(S))
+                        print(f"Tracy-Widom Distribution Values at layer {layer_name}: {tracy_widom_values}", flush=True)
+
                         snr = self.calculate_snr_for_layer(layer_type, layer_number)
                         self.layer_snr[layer_name] = snr
                         print(f"Signal to Noise Ratio at layer {layer_name} = {snr}", flush=True)
-                        print("*"*50, flush=True)
+                        print("*" * 50, flush=True)
 
     def select_layers_for_modification(self, k):
         sorted_layers = sorted(self.layer_snr.items(), key=lambda x: x[1], reverse=False)
         return [layer[0] for layer in sorted_layers[:k]]
-    
+
     def test_and_modify_layers(self, candidate_layers):
         initial_perplexity = self.calculate_model_perplexity()
         print(f"Initial Model Perplexity: {initial_perplexity}")
 
         for layer in candidate_layers:
-            # Modify the layer
             layer_type = layer.split("+")[0]
             layer_number = layer.split("+")[1]
-            self.update_model_reduce_layer(layer_type=layer_type,layer_number=layer_number)
-            
-            # Test the model's performance
+            self.update_model_reduce_layer(layer_type=layer_type, layer_number=layer_number)
+
             new_perplexity = self.calculate_model_perplexity()
             print(f"Tested Model Perplexity after modifying {layer}: {new_perplexity}")
 
-            # If the perplexity does not improve significantly, revert the change
             if new_perplexity > initial_perplexity:
-                self.restore_model_original_layer(layer_type=layer_type,layer_number=layer_number)
+                self.restore_model_original_layer(layer_type=layer_type, layer_number=layer_number)
                 print(f"Reverted changes in {layer} due to lack of improvement.", flush=True)
             else:
                 initial_perplexity = new_perplexity
                 print(f"Modification kept for {layer}. New baseline perplexity: {initial_perplexity}", flush=True)
 
     def save_model(self, save_dir):
-
         self.model.save_pretrained(save_dir)
         self.tokenizer.save_pretrained(save_dir)
+
+    # Additional RMT complexity: Function to retrieve singular values for a specific layer
+    def get_singular_values_for_layer(self, layer_type, layer_number):
+        for name, module in self.model.named_modules():
+            if layer_type in name and str(layer_number) in name:
+                weights = module.weight.double()
+                S = torch.linalg.svdvals(weights)
+                return S
+
+    # Additional RMT complexity: Advanced RMT analysis on a specific layer
+    def advanced_rmt_analysis(self, layer_type, layer_number, singular_values):
+        layer_name = f"{layer_type}+{layer_number}"
+        print(f"Performing Advanced RMT Analysis on layer {layer_name}")
+
+        # Additional RMT complexity: Calculate the average singular value
+        average_s_value = self.average_singular_value(singular_values)
+        print(f"Advanced RMT - Average Singular Value at layer {layer_name}: {average_s_value}", flush=True)
+
+        # Additional RMT complexity: Calculate the Tracy-Widom distribution
+        tracy_widom_values = self.tracy_widom_distribution(beta=1, size=len(singular_values))
+        print(f"Advanced RMT - Tracy-Widom Distribution Values at layer {layer_name}: {tracy_widom_values}", flush=True)
 
 # Usage
 modifier = ModelModifier(model_name)
 
+# %%
 layer_numbers = list(range(31, -1, -1))
 layer_numbers = [f".{l}." for l in layer_numbers]
 print(layer_numbers)
 
-layer_types=['mlp.gate_proj','mlp.down_proj', 'mlp.up_proj', 'self_attn.q_proj', 'self_attn.k_proj', 'self_attn.v_proj', 'self_attn.o_proj']
+layer_types = ['mlp.gate_proj', 'mlp.down_proj', 'mlp.up_proj', 'self_attn.q_proj', 'self_attn.k_proj', 'self_attn.v_proj',
+               'self_attn.o_proj']
 
 modifier.assess_layers_snr(layer_types, layer_numbers)
 top_k_layers = modifier.select_layers_for_modification(15)  # Select top 15 layers
 print(top_k_layers, flush=True)
 
 modifier.test_and_modify_layers(top_k_layers)
-
+# %%
 modifier.save_model("laser_model")
